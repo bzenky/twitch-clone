9e546b5dd70b4c705b1ebc3d749dbad50d3ed77b
bzenky <bzenky@gmail.com>
2023-12-31 12:34:46 -0300

    feat: clerk authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <Button>
-      Shad Button
-    </Button>
+    <div className="flex flex-col gap-y-4">
+      <h1>Dashboard</h1>
+      <UserButton
+        afterSignOutUrl="/"
+      />
+    </div>
   )
 }
