f8e16a95747b40a16395d36d1cbfb0037bf55598
bzenky <bzenky@gmail.com>
2024-01-01 16:27:30 -0300

    feat: dark mode

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,10 +1,14 @@
+import { Logo } from "./_components/logo"
+
 interface Props {
   children: React.ReactNode
 }
 
 export default function AuthLayout({ children }: Props) {
   return (
-    <div className="flex justify-center items-center h-full">
+    <div className="flex flex-col justify-center items-center h-full space-y-6">
+      <Logo />
+
       {children}
     </div>
   )
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from 'next'
 import { ClerkProvider } from '@clerk/nextjs'
 import { Inter } from 'next/font/google'
+import { dark } from '@clerk/themes'
 import './globals.css'
+import { ThemeProvider } from '@/components/theme-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,9 +18,17 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{ baseTheme: dark }}>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ThemeProvider
+            attribute='class'
+            forcedTheme='dark'
+            storageKey='gamehub-theme'
+          >
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   )
